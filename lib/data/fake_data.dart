2cb043ec11b959c77d225ee84b3f419f9e525908
Da hee <ekdudekgml35@naver.com>
2022-05-12 19:07:08 +0900

    fake_data

--- lib/data/fake_data.dart
+++ lib/data/fake_data.dart
@@ -5,6 +5,11 @@ class FakeData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('dd'),
+      ),
+      body: Container(),
+    );
   }
 }
