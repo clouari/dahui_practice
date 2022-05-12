bb4cac93435f7c38934fcfe2c2e2f831cb94e61c
Da hee <ekdudekgml35@naver.com>
2022-05-12 19:10:39 +0900

    이후에 model 부분

--- lib/model/view.dart
+++ lib/model/view.dart
@@ -7,7 +7,7 @@ class View extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('dd'),
+        title: Text('모델입니다'),
       ),
       body: Container(),
     );
