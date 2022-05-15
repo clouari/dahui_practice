375633cd4180134e6aebd1391e8ad1e1bd3cd99c
Da hee <ekdudekgml35@naver.com>
2022-05-15 20:33:04 +0900

    MyAlbumPage

---
+++ lib/ui/album/my_album_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyAlbumPage extends StatefulWidget {
+  const MyAlbumPage({Key? key}) : super(key: key);
+
+  @override
+  _MyAlbumPageState createState() => _MyAlbumPageState();
+}
+
+class _MyAlbumPageState extends State<MyAlbumPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
